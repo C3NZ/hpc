c5445bd67b48820dca7dfeeee2511281d05f7824
C3NZ <cenz@cenz.io>
2020-06-18 14:48:39 -0700

    [add] a script that automates the compilation of our test applications/code.

---
+++ compile_and_run.sh
@@ -0,0 +1,23 @@
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+pushd $ROOT_DIR
+
+export CC=gcc-10.1 CXX=g++-10.1
+
+if [ "$1" = "Release" ] || [ "$1" = "Debug" ]; then
+    echo "Compiling a $1 build."
+    cmake \
+        -DCMAKE_BUILD_TYPE="$1" \
+        -DDISTRIBUTION_BUILD=False .
+elif [ "$1" = "Dist" ]; then
+    echo "Compiling a Distribution build."
+    cmake -DCMAKE_BUILD_TYPE="Release" -DDISTRIBUTION_BUILD=True .
+else
+    echo "Incorrect build type passed."
+    popd
+    exit
+fi
+
+make -j 8
+./"$1"/bin/app
+
+popd  # ROOT_DIR
